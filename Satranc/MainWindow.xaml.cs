c9013fd453939df6668cfd0978724f024dbcb989
Muhammed Yakut <memoaskan@gmail.com>
2022-07-16 10:16:31 +0300

    Buttonların textleri değiştirilebilir kılındı.

--- Satranc/MainWindow.xaml.cs
+++ Satranc/MainWindow.xaml.cs
@@ -18,12 +18,143 @@ namespace Satranc
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
 
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        { 
+            btn1.Content = "BEFORE";
+        }
+        private void btn1_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn1.Content = "AFTER";
+        }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            btn2.Content = "BEFORE";
+        }
+        private void btn2_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn2.Content = "AFTER";
+        }
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            btn3.Content = "BEFORE";
+        }
+        private void btn3_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn3.Content = "AFTER";
+        }
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            btn4.Content = "BEFORE";
+        }
+        private void btn4_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn4.Content = "AFTER";
+        }
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            btn5.Content = "BEFORE";
+        }
+        private void btn5_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn5.Content = "AFTER";
+        }
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            btn6.Content = "BEFORE";
+        }
+        private void btn6_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn6.Content = "AFTER";
+        }
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            btn7.Content = "BEFORE";
+        }
+        private void btn7_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn7.Content = "AFTER";
+        }
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            btn8.Content = "BEFORE";
+        }
+        private void btn8_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn8.Content = "AFTER";
+        }
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            btn9.Content = "BEFORE";
+        }
+        private void btn9_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn9.Content = "AFTER";
+        }
+        private void btn10_Click(object sender, RoutedEventArgs e)
+        {
+            btn10.Content = "BEFORE";
+        }
+        private void btn10_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn10.Content = "AFTER";
+        }
+        private void btn11_Click(object sender, RoutedEventArgs e)
+        {
+            btn11.Content = "BEFORE";
+        }
+        private void btn11_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn11.Content = "AFTER";
+        }
+        private void btn12_Click(object sender, RoutedEventArgs e)
+        {
+            btn12.Content = "BEFORE";
+        }
+        private void btn12_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn12.Content = "AFTER";
+        }
+        private void btn13_Click(object sender, RoutedEventArgs e)
+        {
+            btn13.Content = "BEFORE";
+        }
+        private void btn13_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn13.Content = "AFTER";
+        }
+        private void btn14_Click(object sender, RoutedEventArgs e)
+        {
+            btn14.Content = "BEFORE";
+        }
+        private void btn14_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn14.Content = "AFTER";
+        }
+        private void btn15_Click(object sender, RoutedEventArgs e)
+        {
+            btn15.Content = "BEFORE";
+        }
+        private void btn15_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn15.Content = "AFTER";
+        }
+        private void btn16_Click(object sender, RoutedEventArgs e)
+        {
+            btn16.Content = "BEFORE";
+        }
+
+        private void btn16_MouseRightButtonClick(object sender, RoutedEventArgs e)
+        {
+            btn16.Content = "AFTER";
         }
     }
 }
